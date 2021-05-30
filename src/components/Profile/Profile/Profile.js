7ed0303c67fe0a5b28c9e97de4c27dc065eb5c5d
Badhan112 <badhancsesastc112@gmail.com>
2021-05-30 19:33:45 +0600

    add post details with respective comments

--- src/components/Profile/Profile/Profile.js
+++ src/components/Profile/Profile/Profile.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { UserContext } from '../../../App';
 
 const Profile = () => {
-    
+    const user = useContext(UserContext);
+    const { name, username, email, phone, website } = user;
     return (
-        <div>
-            Profile
-        </div>
+        <main>
+            <section className='container'>
+                <h3>{name}</h3>
+            </section>
+        </main>
     );
 };
 
--- src/components/SinglePost/SinglePost/SinglePost.js
+++ src/components/SinglePost/SinglePost/SinglePost.js
@@ -4,19 +4,30 @@ import CommentCart from '../CommentCart/CommentCart';
 
 const SinglePost = () => {
     let { id } = useParams();
+    const [post, setPost] = useState({});
     const [comments, setComments] = useState([]);
 
 
     useEffect(() => {
+        fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+            .then(res => res.json())
+            .then(data => setPost(data));
+
         fetch(`https://jsonplaceholder.typicode.com/posts/${id}/comments`)
-        .then(res => res.json())
-        .then(data => setComments(data));
+            .then(res => res.json())
+            .then(data => setComments(data));
     }, [id])
 
     return (
         <main className='my-5'>
-            <h1 className='text-center'>All Comments</h1>
-            <section className='row container mx-auto'>
+            <section className='container p-4'>
+                <div className='shadow p-4 rounded-3'>
+                    <h2>{post.title}</h2>
+                    <p>{post.body}</p>
+                </div>
+            </section>
+            <section className='container mx-auto'>
+                <h3 className='text-center'>All Comments</h3>
                 {
                     comments.map(comment => <CommentCart name={comment.name} email={comment.email} body={comment.body} />)
                 }
